0c24f86f8c4349a5c58255bf2ae87fd4f1d4bd2f
Yahyachandraa <81281495+Yahyachandraa@users.noreply.github.com>
2021-03-24 17:22:36 +0000

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,12 @@ let sender;
 if (url.searchParams.get('by') != null) {
   sender = url.searchParams.get('by');
 } else {
-  sender = "Yahya Chandra";
+  sender = "YahyaChandra";
 }
 
 let footer = document.getElementById("credit");
 footer.innerHTML = sender;
-footer.href = "https://www.instagram.com/tahi_lalat06/";
+footer.href = "https://www.instagram.com/tahilalat06/";
 
 document.querySelector(".tombol").addEventListener('click', function () {
   Swal.fire("Hallo Sayangku", "Aku ada pertanyaan nih buat kamu?", "question").then(function () {
